1f33f7fb93ee66886b94029e84947ca49b852ca4
loser_ruza <t.m.seth2000@gmail.com>
2022-02-07 14:41:59 +0900

    Changed the HTML for the site.

--- blog/sleep.py
+++ blog/sleep.py
@@ -1,7 +1,7 @@
-import fitbit
-import datetime
-import pandas as pd
-from ast import literal_eval
+import fitbit #FitbitAPI用
+import datetime #日付取得用
+import pandas as pd #消しても大丈夫かも
+from ast import literal_eval #文字列処理用
 
 # tokenファイルを上書きする関数
 def updateToken(token):
@@ -11,8 +11,8 @@ def updateToken(token):
     return
 
 # ユーザ情報の定義
-CLIENT_ID =  '23BC6R'
-CLIENT_SECRET  = 'b888b16df4a934f27ce4267964d19223'
+CLIENT_ID =  '#####'
+CLIENT_SECRET  = '#####'
 TOKEN_FILE = "token.txt"
 
 # ファイルからtoken情報を読み込む
@@ -32,13 +32,38 @@ client = fitbit.Fitbit(CLIENT_ID, CLIENT_SECRET,
 now = datetime.datetime.now()
 today = str(now.year) + "-" + str(now.month) + "-" + str(now.day)
 
-# 睡眠効率取得
-dataTxt = str(client.sleep(date = today))
+# 睡眠データ取得
+dataTxt = str(client.sleep(date = today)
 
+#睡眠データから実際に寝た時間とベッドにいた時間を切り抜く
+#例外処理：睡眠時間が100分未満のとき、切り抜く範囲を2桁にする
+#例外処理：念のため切り取った部分が数値でなかったとき、睡眠効率を0にしエラー画面にする
 minutesAsleepAd = dataTxt.find('minutesAsleep')
 minutesAsleep = dataTxt[minutesAsleepAd+16:minutesAsleepAd+19]
+try:
+    minutesAsleep = int(minutesAsleep)
+except ValueError:
+    try:
+        minutesAsleep = int(dataTxt[minutesAsleepAd+16:minutesAsleepAd+18])
+    except ValueError:
+        minutesAsleep = 0
 
 timeInBedAd = dataTxt.find('timeInBed')
 timeInBed = dataTxt[timeInBedAd+12:timeInBedAd+15]
+try:
+    timeInBed = int(timeInBed)
+except ValueError:
+    try:
+        timeInBed = int(dataTxt[timeInBedAd+12:timeInBedAd+14])
+    except ValueError:
+        timeInBed = 1
 
-sleepEfficiency = int(int(minutesAsleep) / int(timeInBed) * 100)
+try:
+    sleepEfficiency = int(minutesAsleep / timeInBed * 100)
+except ValueError:
+    sleepEfficiency = 0
+
+#print(dataTxt)
+#print(minutesAsleep)
+#print(timeInBed)
+#print(sleepEfficiency)
--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,14 @@
-from django.shortcuts import render
-from . import sleep1, sleep2, sleep3, sleep4, sleep5, sleep6, sleep7, sleep8, sleep9, sleep10
+from django.core.cache import cache
+cache.clear() #キャッシュクリア
 
-import cairo
+from django.shortcuts import render #ページ遷移用
+from . import sleep
+
+import cairo #モンドリアン風画像作成用
 import random
-import cv2  # OpenCVのインポート
+import cv2 #明度変更用
+
+import datetime #ログイン日記録用
 
 # the size of the image
 IMAGE_WIDTH = 500
@@ -21,6 +26,8 @@ tiles = {}
 #colors (0,white),(1,black),(2,red),(3,yellow)(4,blue)
 
 # Create your views here.
+
+#ログインID/Passを複数の関数で使用
 ID = 'ID'
 Pass = 'Pass'
 
@@ -28,49 +35,91 @@ Pass = 'Pass'
 def login(request):
     # POST
     if request.method == 'POST':
+        #記入されたID/Passを取得
         userid = request.POST.get('userid')
         password = request.POST.get('password')
 
+        #送信ボタンが押されたとき
         if 'buttom' in request.POST:
             for i in range(1, 15):
+                #ID/Passが一致したときログイン、主観評価記録用にページ遷移
                 if userid == 'sleep%d' %i and password == 'sleep%d' %i:
                     global ID
                     global Pass
                     ID = 'sleep%d' %i
                     Pass = 'sleep%d' %i
                     return render(request, 'blog/choice.html', {})
+
+            #ID/Passが間違えているときページ遷移しない
             return render(request, 'blog/login.html', {})
     else:
         return render(request, 'blog/login.html', {})
 
 def choice(request):
     if request.method == 'POST':
+
+        #主観評価送信された日時取得
+        dt_now = str(datetime.datetime.now())
+        #主観評価取得
         state = int(request.POST.get('syukan'))
+        #ファイル記録用主観評価取得
+        file_state =str(request.POST.get('syukan'))
+
+        fp = open(r'C:\Users\tmset\djangogirls\static\file.txt', 'a') #ファイル書き込み
+        fp.write(dt_now) #アクセス時刻を記録
+        fp.write('\n')
+        fp.write(ID) #IDを記録
+        fp.write('\n')
+        fp.write(file_state) #主観評価を記録
+        fp.write('\n')
+        fp.close()
+
+        #アカウントごとの睡眠効率を取得
+        if ID == 'sleep1' and Pass == 'sleep1':
+            sleepEfficiency = sleep1.sleepEfficiency
+        elif ID == 'sleep2' and Pass == 'sleep2':
+            sleepEfficiency = sleep2.sleepEfficiency
+        elif ID == 'sleep3' and Pass == 'sleep3':
+            sleepEfficiency = sleep3.sleepEfficiency
+        elif ID == 'sleep4' and Pass == 'sleep4':
+            sleepEfficiency = sleep4.sleepEfficiency
+        elif ID == 'sleep5' and Pass == 'sleep5':
+            sleepEfficiency = sleep5.sleepEfficiency
+        elif ID == 'sleep6' and Pass == 'sleep6':
+            sleepEfficiency = sleep6.sleepEfficiency
+        elif ID == 'sleep7' and Pass == 'sleep7':
+            sleepEfficiency = sleep7.sleepEfficiency
+        elif ID == 'sleep8' and Pass == 'sleep8':
+            sleepEfficiency = sleep8.sleepEfficiency
+        elif ID == 'sleep9' and Pass == 'sleep9':
+            sleepEfficiency = sleep9.sleepEfficiency
+        elif ID == 'sleep10' and Pass == 'sleep10':
+            sleepEfficiency = sleep10.sleepEfficiency
+        elif ID == 'sleep11' and Pass == 'sleep11':
+            sleepEfficiency = sleep11.sleepEfficiency
+        elif ID == 'sleep12' and Pass == 'sleep12':
+            sleepEfficiency = sleep12.sleepEfficiency
+        elif ID == 'sleep13' and Pass == 'sleep13':
+            sleepEfficiency = sleep13.sleepEfficiency
+        elif ID == 'sleep14' and Pass == 'sleep14':
+            sleepEfficiency = sleep14.sleepEfficiency
+        elif ID == 'sleep15' and Pass == 'sleep15':
+            sleepEfficiency = sleep15.sleepEfficiency
+
+        #時々sleepEfficiencyが空になるバグがあるので、その場合エラー画面に遷移
+        try:
+            sleepEfficiency = sleepEfficiency
+        except UnboundLocalError:
+            return render(request,'blog/retry.html',{})
+
+        #日付が変わってからまだ寝ていないときもエラー画面に繊維
+        if sleepEfficiency == 0:
+            return render(request,'blog/nodata.html',{})
+
+        #送信ボタンが押されたとき
         if 'buttom' in request.POST:
-            if ID == 'sleep1' and Pass == 'sleep1':
-                sleepEfficiency = sleep1.sleepEfficiency
-            elif ID == 'sleep2' and Pass == 'sleep2':
-                sleepEfficiency = sleep2.sleepEfficiency
-            elif ID == 'sleep3' and Pass == 'sleep3':
-                sleepEfficiency = sleep3.sleepEfficiency
-            elif ID == 'sleep4' and Pass == 'sleep4':
-                sleepEfficiency = sleep4.sleepEfficiency
-            elif ID == 'sleep5' and Pass == 'sleep5':
-                sleepEfficiency = sleep5.sleepEfficiency
-            elif ID == 'sleep6' and Pass == 'sleep6':
-                sleepEfficiency = sleep6.sleepEfficiency
-            elif ID == 'sleep7' and Pass == 'sleep7':
-                sleepEfficiency = sleep7.sleepEfficiency
-            elif ID == 'sleep8' and Pass == 'sleep8':
-                sleepEfficiency = sleep8.sleepEfficiency
-            elif ID == 'sleep9' and Pass == 'sleep9':
-                sleepEfficiency = sleep9.sleepEfficiency
-            elif ID == 'sleep10' and Pass == 'sleep110':
-                sleepEfficiency = sleep10.sleepEfficiency
-
-            print(sleepEfficiency)
-
-            if state==1:
+            #主観評価と客観評価のズレ、ページ遷移用変数を設定
+            if state==1 or state=='１':
                 if sleepEfficiency>=90:
                     gap = 0
                     stage = 1
@@ -80,7 +129,7 @@ def choice(request):
                 elif sleepEfficiency<85:
                     gap = 2
                     stage = 3
-            elif state==2:
+            elif state==2 or state=='２':
                 if sleepEfficiency>=90:
                     gap = 1
                     stage = 4
@@ -90,7 +139,7 @@ def choice(request):
                 elif sleepEfficiency<85:
                     gap = 1
                     stage = 6
-            elif state==3:
+            elif state==3 or state=='３':
                 if sleepEfficiency>=90:
                     gap = 2
                     stage = 7
@@ -100,7 +149,11 @@ def choice(request):
                 elif sleepEfficiency<85:
                     gap = 0
                     stage = 9
+            else:
+                return render(request,'blog/choice.html',{})
 
+
+            #ズレに応じたモンドリアン風画像のボーダー数選択
             if gap==0:
                 MAX_LINES = 10
                 MIN_LINES = 8
@@ -110,36 +163,46 @@ def choice(request):
             elif gap==2:
                 MAX_LINES = 25
                 MIN_LINES = 20
+
+            #睡眠効率から明度算出
             Value = sleepEfficiency/100
+
+            #モンドリアン風画像作成
             generate_tiles(MIN_LINES, MAX_LINES)
             draw_map()
             changeValue(Value)
 
-            if stage==1:
-                return render(request,'blog/output1.html',{})
-            elif stage==2:
-                return render(request,'blog/output2.html',{})
-            elif stage==3:
-                return render(request,'blog/output3.html',{})
-            elif stage==4:
-                return render(request,'blog/output4.html',{})
-            elif stage==5:
-                return render(request,'blog/output5.html',{})
-            elif stage==6:
-                return render(request,'blog/output6.html',{})
-            elif stage==7:
-                return render(request,'blog/output7.html',{})
-            elif stage==8:
-                return render(request,'blog/output8.html',{})
-            elif stage==9:
-                return render(request,'blog/output9.html',{})
-
+            #ページ遷移
+            #(outputA1～9:文章、outputB1～9:画像、outputC1～9:文章＋画像)
+            for i in range(1, 16):
+                if userid == 'sleep%d' %i and password == 'sleep%d' %i:
+                    if stage==1:
+                        return render(request,'blog/outputC1.html',{})
+                    elif stage==2:
+                        return render(request,'blog/outputC2.html',{})
+                    elif stage==3:
+                        return render(request,'blog/outputC3.html',{})
+                    elif stage==4:
+                        return render(request,'blog/outputC4.html',{})
+                    elif stage==5:
+                        return render(request,'blog/outputC5.html',{})
+                    elif stage==6:
+                        return render(request,'blog/outputC6.html',{})
+                    elif stage==7:
+                        return render(request,'blog/outputC7.html',{})
+                    elif stage==8:
+                        return render(request,'blog/outputC8.html',{})
+                    elif stage==9:
+                        return render(request,'blog/outputC9.html',{})
+                        
     else:
-         return render(request,'blog/choice.html',{})
+        return render(request,'blog/choice.html',{})
 
+#なくてもいいかもしれない関数
 def output(request):
     return render(request, 'blog/output.html', {})
 
+#モンドリアン風画像の白紙作成
 def generate_tiles(MIN_LINES, MAX_LINES):
     # build tile map
     for x in range(MAP_WIDTH):
@@ -148,6 +211,7 @@ def generate_tiles(MIN_LINES, MAX_LINES):
             tiles[x,y] = 0
     draw_lines(MIN_LINES, MAX_LINES)
 
+#白紙にボーダー作成
 def draw_lines(MIN_LINES, MAX_LINES):
     total_lines = random.randint(MIN_LINES,MAX_LINES)
     for h in range(int(total_lines/2)):
@@ -160,6 +224,7 @@ def draw_lines(MIN_LINES, MAX_LINES):
             tiles[x,y] = 1
     fill_rects()
 
+#着色するマスを選択
 def fill_rects():
     total_rects = random.randint(MIN_RECTS,MAX_RECTS)
     max_iters = 5
@@ -171,6 +236,7 @@ def fill_rects():
                 color = random.randint(2,4)
                 flood_recursion(x,y,0,color)
 
+#着色
 def flood_recursion(x,y,start_color,update_color):
     width = MAP_WIDTH
     height = MAP_HEIGHT
@@ -185,6 +251,7 @@ def flood_recursion(x,y,start_color,update_color):
             if 0 <= n[0] <= width-1 and 0 <= n[1] <= height-1:
                 flood_recursion(n[0],n[1],start_color,update_color)
 
+#数値による着色をRGBに変換し画像保存
 def draw_map():
     # draw tile map using pycairo
     surface = cairo.ImageSurface(cairo.FORMAT_RGB24, IMAGE_WIDTH, IMAGE_HEIGHT)
@@ -206,6 +273,7 @@ def draw_map():
             ctx.fill()
     surface.write_to_png(r'C:\Users\tmset\djangogirls\static\mondrian.png')
 
+#帆陣された画像を睡眠効率に応じて明度変更し画像保存
 def changeValue(Value):
     img = cv2.imread(r'C:\Users\tmset\djangogirls\static\mondrian.png')  # 画像の読み出し
     img_hsv = cv2.cvtColor(img,cv2.COLOR_BGR2HSV)  # 色空間をBGRからHSVに変換
